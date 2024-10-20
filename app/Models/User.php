003a9c2149dd6adf108cff3f607265d71a0b233a
Bennet Otoho <bennet2000@outlook.com>
2024-10-20 15:59:30 +0100

    trigger `UserModified` event on user update

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Events\UserModified;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -43,4 +45,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::updated(function ($user) {
+            // TODO: add 'email' and implement calls to recreate the user in third-party provider
+            if ($user->isDirty(['name', 'time_zone'])) {
+                event(new UserModified($user));
+            }
+        });
+    }
 }
