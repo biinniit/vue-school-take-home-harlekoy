ff4b67fda1ce8fc15776c1bba5d3bc48a7f1acb7
Bennet Otoho <bennet2000@outlook.com>
2024-10-20 03:39:27 +0100

    create `app:reset-user` command

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,7 +31,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'timezone' => $this->faker->randomElement(static::$timezones),
+            'timezone' => fake()->randomElement(static::$timezones),
         ];
     }
 
@@ -43,5 +43,10 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public static function getTimezones(): array
+    {
+        return static::$timezones;
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('app:reset-user {user : The ID of the user}', function (string $user) {
+    $userObj = User::find($user);
+    $userObj->name = fake()->name();
+    $userObj->timezone = fake()->randomElement(UserFactory::getTimezones());
+    $userObj->save();
+})->purpose('Reset user\'s firstname, lastname, and timezone randomly');
